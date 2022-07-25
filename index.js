95a5468025e81a9318f974a34561500216b22ee7
Juan2579 <juandavidgualdronnavarro@gmail.com>
2022-07-24 23:00:33 -0500

    Added mobile navbar with animation

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const menuHamburger = document.querySelector(".navbar_menu")
 const menuClose = document.querySelector(".navbar_close")
+const menuHidden = document.querySelector(".navbar_mobile-hidden")
 
 const openNavigation = () => {
     menuHamburger.style.display = "none"
 
     menuClose.style.display = "block"
+
+    menuHidden.classList.add("shown")
+    menuHidden.classList.remove("hidden")
+
 }
 const closeNavigation = () => {
     menuHamburger.style.display = "block"
 
     menuClose.style.display = "none"
+
+    menuHidden.classList.remove("shown")
+    menuHidden.classList.add("hidden")
+    
 }
 
